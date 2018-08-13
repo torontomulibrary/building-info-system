92116389e7a2d1b9cd73dccb27d32a9db25c1c23
David Quast <dquast@ryerson.ca>
2018-08-13 14:13:23 -0400

    - Add tab capture - Remove some unused code - Move class changes into `hostData()` - Add some error checks to detail strings

--- src/components/detail-panel/detail-panel.tsx
+++ src/components/detail-panel/detail-panel.tsx
@@ -1,11 +1,10 @@
-import { Component, Event, EventEmitter, Element, Prop, State, Watch } from '@stencil/core';
-//import { MDCTemporaryDrawer } from '@material/drawer';
-
+import { Component, Element, Prop, State, Listen } from '@stencil/core';
 import {
   LazyStore,
   MapElementDetail,
-  // MapElement,
 } from '../../interface';
+
+import { FOCUSABLE_ELEMENTS } from '../../global/constants';
 
 import map from '../../reducers/map-reducer';
 
@@ -17,26 +16,25 @@ import {
   tag: 'rula-detail-panel',
   styleUrl: 'detail-panel.scss',
   host: {
-    theme: 'rula-detail-panel rula-detail-panel--animated'
+    theme: 'rula-detail-panel'
   }
 })
 
 export class DetailPanel {
-  //private _drawer: MDCTemporaryDrawer;
   private _storeUnsubscribe: Function;
+
+  private _firstTabStop: HTMLElement;
+  private _lastTabStop: HTMLElement;
+
+  private _oldTabStop: HTMLElement;
 
   @Element() root: HTMLStencilElement;
 
+  @State() _animated = false;
   @State() _details: MapElementDetail[] = [];
   @Prop({ mutable: true }) open: boolean;
-  @Watch('open')
-  openDrawer() {
-    //this._drawer.open = this.open;
-  }
 
   @Prop({ context: 'lazyStore' }) lazyStore: LazyStore;
-
-  @Event() drawerClose: EventEmitter;
 
   async componentWillLoad() {
     this.lazyStore.addReducers({map});
@@ -45,55 +43,114 @@ export class DetailPanel {
     );
   }
 
-  componentDidLoad() {
-    // this._drawer = new MDCTemporaryDrawer(this.root);
-    // this._drawer.listen('MDCTemporaryDrawer:close', () => {
-    //   this.root.removeAttribute('open');
-    //   this.drawerClose.emit();
-    //   //this.oldTabStop.focus();
-    // });
-
-    // this._drawer.listen('MDCTemporaryDrawer:open', () => {
-    //   //this.setFocusTrap_();
-    // });
-  }
-
   componentDidUnload() {
     this._storeUnsubscribe();
   }
 
-  _create
+  @Listen('keydown.tab')
+  handleTab(evt: KeyboardEvent) {
+    var TAB_KEYCODE = 9;
+
+    if (this.open && evt.keyCode === TAB_KEYCODE) {
+      if (evt.shiftKey) {
+        if (this._firstTabStop && evt.target === this._firstTabStop) {
+          evt.preventDefault();
+          this._lastTabStop.focus();
+        }
+      } else {
+        if (this._lastTabStop && evt.target === this._lastTabStop) {
+          evt.preventDefault();
+          this._firstTabStop.focus();
+        }
+      }
+    }
+  }
+
+  _setFocusTrap() {
+    var focusableElements = this.root.querySelectorAll(FOCUSABLE_ELEMENTS);
+
+    if (focusableElements.length > 0) {
+      this._firstTabStop = focusableElements[0] as HTMLElement;
+      this._lastTabStop = focusableElements[focusableElements.length - 1] as HTMLElement;
+    } else {
+      // Reset saved tab stops when there are no focusable elements in the card.
+      this._firstTabStop = null;
+      this._lastTabStop = null;
+    }
+
+    this._oldTabStop = document.activeElement as HTMLElement;
+  }
 
   _stateChanged(state) {
     if (state.activeElement && state.activeElement.details) {
       this._details = Object.values(state.activeElement.details);
-      this.root.forceUpdate();
+      this.root.addEventListener('transitionend', this._handleTransitionEnd);
+      this._animated = true;
       this.open = true;
-      this.root.classList.add('rula-detail-panel--open');
+      this._setFocusTrap();
+
+      this._firstTabStop.focus();
     } else {
+      this.root.addEventListener('transitionend', this._handleTransitionEnd);
+      this._animated = true;
       this.open = false;
-      this.root.classList.remove('rula-detail-panel--open');
+
+      if (this._oldTabStop) {
+        this._oldTabStop.focus();
+      }
+    }
+  }
+
+  _handleTransitionEnd(evt) {
+    if (this.root === evt.target) {
+      this._animated = false;
+      this.root.removeEventListener('transitionend', this._handleTransitionEnd);
+    }
+  }
+
+  hostData() {
+    return {
+      class: {
+        'rula-detail-panel--open': this.open,
+        'rula-detail-panel--animated': this._animated
+      }
     }
   }
 
   render() {
-    let detailName = this._details.map((detail:MapElementDetail) => {
-      return detail.name;
-    }).join(', ');
+    let detail = this._details[0];
+    // For now assume that a MapElement has only one detail.  While it is
+    // possible for an element to have more than one detail, for now it's not
+    // supported from the front-end display.
 
     return ([
-        <header class='rula-detail-panel__header'>
+        <header class="rula-detail-panel__header">
           <span class="rula-detail-panel__title">
-            <div class='mdc-typography--body2'>{this._details[0].code}</div>
-            <div class='mdc-typography--headline6'>{detailName}</div>
+            <div class="mdc-typography--body2">{detail && detail.code || ''}</div>
+            <div class="mdc-typography--headline6">{detail && detail.name || ''}</div>
           </span>
-          <button class='material-icons rula-detail-panel__close' onClick={_ => {this.lazyStore.dispatch(updateActiveElement(undefined));}}>close</button>
+          <button
+              class="material-icons rula-detail-panel__close"
+              onClick={_ => {this.lazyStore.dispatch(updateActiveElement(undefined));}}
+              aria-label="Close detail panel.">
+            close
+          </button>
         </header>,
-        <div class='rula-detail-panel__content'>
+        <div class="rula-detail-panel__content">
           <div class="rula-detail-panel__section">
             <div class="rula-detail-panel__subtitle mdc-typography--subtitle2">Description</div>
-            {this._details[0].description}
+            {detail && detail.description || ''}
           </div>
+          {/* <div class="rula-detail-panel__section">
+            <button
+                class="mdc-button rula-detail-panel__action"
+                aria-label={`Get directions to ${detail.code}`}>
+                <i class="material-icons mdc-button__icon" aria-hidden="true">
+                  directions
+                </i>
+              Directions
+            </button>
+          </div> */}
         </div>
     ]);
   }
---
+++ src/global/constants.ts
@@ -0,0 +1,5 @@
+let FOCUSABLE_ELEMENTS = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex], [contenteditable]';
+
+export {
+  FOCUSABLE_ELEMENTS
+};
