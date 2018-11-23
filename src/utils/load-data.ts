b70f83ad27ff9b2e0405e22182705211d01faf92
David Quast <dquast@ryerson.ca>
2018-11-23 08:32:12 -0500

    feat(local-data): Add support for skipping Local Storage lookup

--- src/utils/load-data.ts
+++ src/utils/load-data.ts
@@ -3,17 +3,27 @@ import { fetchJSON } from '../utils/fetch';
 
 import { get, set } from './local-storage';
 
-export function loadData<T>(key: string, type: string) {
-  return get(key).then((res: T) => {
-    if (res) {
-      return Promise.resolve(res);
-    } else {
-      return fetchJSON(API_URL + type).then((data: T) => {
-        set(key, data);
-        return Promise.resolve(data);
-      }, reason => {
-        return Promise.reject(reason);
-      });
+function fetchWrapper(type: string, key?: string) {
+  return fetchJSON(API_URL + type).then(data => {
+    if (key) {
+      set(key, data);
     }
+    return Promise.resolve(data);
+  }, reason => {
+    return Promise.reject(reason);
   });
+}
+
+export function loadData<T>(type: string, key?: string) {
+  if (key) {
+    return get(key).then((res: T) => {
+      if (res) {
+        return Promise.resolve(res);
+      } else {
+        return fetchWrapper(type, key);
+      }
+    });
+  } else {
+    return fetchWrapper(type);
+  }
 }
