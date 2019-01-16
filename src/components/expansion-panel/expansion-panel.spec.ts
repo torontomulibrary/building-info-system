3689b025f9a7733a9b8477f260edaadd1510394e
David Quast <dquast@ryerson.ca>
2019-01-16 11:09:17 -0500

    fix(expansion-panel): Add basic test file.

---
+++ src/components/expansion-panel/expansion-panel.spec.ts
@@ -0,0 +1,7 @@
+import { RLExpansionPanel } from './expansion-panel';
+
+describe('expansion-panel', () => {
+  it('should build', () => {
+    expect(new RLExpansionPanel()).toBeTruthy();
+  });
+});
