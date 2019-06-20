fb5c0ccd3342f2b1d6c202cc8232842890febd47
David Quast <dquast@ryerson.ca>
2019-06-20 13:37:17 -0400

    refactor(view-book): commented out view-book, may not be needed

--- src/views/view-book/view-book.tsx
+++ src/views/view-book/view-book.tsx
@@ -1,116 +1,114 @@
-import { Component, Element, Host, Prop, State, h } from '@stencil/core';
-import { QueueApi } from '@stencil/core/dist/declarations';
-import { RouterHistory } from '@stencil/router';
+// import { Component, Element, Host, Prop, State, h } from '@stencil/core';
+// import { QueueApi } from '@stencil/core/dist/declarations';
+// import { RouterHistory } from '@stencil/router';
 
-import { Card } from '../../components/card/card';
-import { BASE_URL } from '../../global/config';
-import { APP_DATA, CLUSTER_TYPE, ROUTES } from '../../global/constants';
-import { SearchHistory } from '../../interface';
-import { dataService } from '../../utils/data-service';
+// import { APP_DATA } from '../../global/constants';
+// import { SearchHistory } from '../../interface';
+// import { dataService } from '../../utils/data-service';
 
-@Component({
-  tag: 'view-book',
-  styleUrl: 'view-book.scss',
-})
+// @Component({
+//   tag: 'view-book',
+//   styleUrl: 'view-book.scss',
+// })
 
-export class ViewBooks {
-  @Element() root!: HTMLElement;
+// export class ViewBooks {
+//   @Element() root!: HTMLElement;
 
-  @State() searches?: SearchHistory;
+//   @State() searches?: SearchHistory;
 
-  @State() loaded = false;
-  @State() inDom = false;
-  @State() clusterColumns = 5;
+//   @State() loaded = false;
+//   @State() inDom = false;
+//   @State() clusterColumns = 5;
 
-  // @Prop({ mutable: true }) appData!: AppData;
+//   // @Prop({ mutable: true }) appData!: AppData;
 
-  @Prop() history!: RouterHistory;
+//   @Prop() history!: RouterHistory;
 
-  @Prop() appLoaded = false;
+//   @Prop() appLoaded = false;
 
-  @Prop({ context: 'queue' }) queue!: QueueApi;
+//   @Prop({ context: 'queue' }) queue!: QueueApi;
 
-  componentWillLoad() {
-    this.searches = dataService.getData(APP_DATA.HISTORY);
-  }
+//   componentWillLoad() {
+//     this.searches = dataService.getData(APP_DATA.HISTORY);
+//   }
 
-  componentDidLoad() {
-    this.checkSize();
-  }
+//   componentDidLoad() {
+//     this.checkSize();
+//   }
 
-  checkSize() {
-    if (this.root.offsetHeight === 0) {
-      this.queue.write(() => {
-        this.checkSize();
-      });
-    } else {
-      this.loaded = true;
-      this.updateWidth();
-    }
-  }
+//   checkSize() {
+//     if (this.root.offsetHeight === 0) {
+//       this.queue.write(() => {
+//         this.checkSize();
+//       });
+//     } else {
+//       this.loaded = true;
+//       this.updateWidth();
+//     }
+//   }
 
-  updateWidth() {
-    const width = this.root.clientWidth - 128;
-    this.clusterColumns = 700 > width ? 3 : 928 > width ? 4 : 1160 > width ? 5 : 1392 > width ? 6 : 1624 > width ? 7 : 8;
-  }
+//   updateWidth() {
+//     const width = this.root.clientWidth - 128;
+//     this.clusterColumns = 700 > width ? 3 : 928 > width ? 4 : 1160 > width ? 5 : 1392 > width ? 6 : 1624 > width ? 7 : 8;
+//   }
 
-  _cardClicked(e: CustomEvent) {
-    e.preventDefault();
+//   _cardClicked(e: CustomEvent) {
+//     e.preventDefault();
 
-    const card: Card = e.detail;
-    this.history.push(`${BASE_URL}${ROUTES.SEARCH}/${card.cardData}`);
-  }
+//     // const card: Card = e.detail;
+//     // this.history.push(`${BASE_URL}${ROUTES.SEARCH}/${card.cardData}`);
+//   }
 
-  render() {
-    // Render resent searches and popular books.
-    if (this.searches) {
-      return (
-        <Host style={{
-          opacity: (this.loaded && this.appLoaded) ? '1' : '0',
-        }} class={{
-          'rl-view': true,
-          'rl-view--book': true,
-          'rl-view--transition': this.inDom,
-        }}>
-          <stencil-route-title pageTitle="Books" />,
-          <div class="mdc-layout-grid">
-            <div class="mdc-layout-grid__inner">
-              <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-2-desktop"></div>
-              <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-8-desktop">
-                Welcome to the Ryerson Library Book Finder.  Use it to find where the
-                book you're looking for is in the Library.  Below are some common search
-                terms and popular books.  To look for a specific book, use the search
-                bar above.
-                <slot />
-              </div>
-            </div>
-          </div>
-          <rl-cluster
-            heading="Recent Books"
-            type={CLUSTER_TYPE.CARD}
-            columns={this.clusterColumns}
-            data={this.searches.recent}>
-          </rl-cluster>
-          <rl-cluster
-            heading="Popular Books"
-            type={CLUSTER_TYPE.CARD}
-            columns={this.clusterColumns}
-            data={this.searches.popular}>
-          </rl-cluster>
-        </Host>
-      );
-    }
+//   render() {
+//     // Render resent searches and popular books.
+//     if (this.searches) {
+//       return (
+//         <Host style={{
+//           opacity: (this.loaded && this.appLoaded) ? '1' : '0',
+//         }} class={{
+//           'rl-view': true,
+//           'rl-view--book': true,
+//           'rl-view--transition': this.inDom,
+//         }}>
+//           <stencil-route-title pageTitle="Books" />,
+//           <div class="mdc-layout-grid">
+//             <div class="mdc-layout-grid__inner">
+//               <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-2-desktop"></div>
+//               <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-8-desktop">
+//                 Welcome to the Ryerson Library Book Finder.  Use it to find where the
+//                 book you're looking for is in the Library.  Below are some common search
+//                 terms and popular books.  To look for a specific book, use the search
+//                 bar above.
+//                 <slot />
+//               </div>
+//             </div>
+//           </div>
+//           <rl-cluster
+//             heading="Recent Books"
+//             type={CLUSTER_TYPE.CARD}
+//             columns={this.clusterColumns}
+//             data={this.searches.recent}>
+//           </rl-cluster>
+//           <rl-cluster
+//             heading="Popular Books"
+//             type={CLUSTER_TYPE.CARD}
+//             columns={this.clusterColumns}
+//             data={this.searches.popular}>
+//           </rl-cluster>
+//         </Host>
+//       );
+//     }
 
-    return (
-      <Host style={{
-        opacity: (this.loaded && this.appLoaded) ? '1' : '0',
-      }} class={{
-        'rl-view': true,
-        'rl-view--book': true,
-        'rl-view--transition': this.inDom,
-      }}>
-        <div>Loading...</div>
-      </Host>
-    );
-  }
-}
+//     return (
+//       <Host style={{
+//         opacity: (this.loaded && this.appLoaded) ? '1' : '0',
+//       }} class={{
+//         'rl-view': true,
+//         'rl-view--book': true,
+//         'rl-view--transition': this.inDom,
+//       }}>
+//         <div>Loading...</div>
+//       </Host>
+//     );
+//   }
+// }
